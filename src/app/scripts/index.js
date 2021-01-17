8972d23d6163a1850a3b6481117eb3f718241663
ThibaudL <thibaud.lamarche@gmail.com>
2021-01-17 19:29:23 +0100

    refacto mise en forme ajout fichier common.css pour tout ce qui est commun aux pages

--- src/app/scripts/index.js
+++ src/app/scripts/index.js
@@ -1,46 +1,22 @@
-function createSpanFortAttribute(attribute, content) {
-    const title = document.createElement('span');
-    const titleClass = document.createAttribute('class');
-    titleClass.value = attribute;
-    title.setAttributeNode(titleClass);
-    title.appendChild(content)
-    return title;
+function bookToLine(book) {
+    return `<li>
+        <a class="book" href="ajout_livre.html?isbn=${book.industryIdentifiers[0].identifier}">
+            <span class="title">${book.title}</span>
+            <span class="authors">${book.authors.join(', ')}</span>
+            <span class="description" title="${book.description}"><i class="fas fa-info-circle"></i></span>
+        </a>
+    </li>`
 }
 
-function createIcon(classes) {
-    let infobulle = document.createElement('i');
-    const titleClass = document.createAttribute('class');
-    titleClass.value = classes;
-    infobulle.setAttributeNode(titleClass);
-    return infobulle;
-}
-
-function attachTitle(destination, title) {
-    const titleDescription = document.createAttribute('title');
-    destination.setAttributeNode(titleDescription);
-    titleDescription.value = title;
-    return destination;
-}
-
-function getBooks(){
+function getBooks() {
     fetch('/books')
         .then((res) => res.json())
         .then((books = []) => {
-            console.log(books);
-            books.map((book) => {
-                const li = document.createElement('li');
-                li.appendChild(createSpanFortAttribute('title', document.createTextNode(book.title)));
-                li.appendChild(createSpanFortAttribute('authors', document.createTextNode(book.authors.join(', '))));
-                li.appendChild(createSpanFortAttribute('description', attachTitle(createIcon("fas fa-info-circle"), book.description)));
-                li.appendChild(createSpanFortAttribute('book-infos', createIcon("fas fa-book-open")));
-                return li;
-            })
-            .forEach((li) => {
-                document.getElementById('to-read')
-                    .appendChild(li)
-            });
-
-        })
+            document.getElementById('to-read').innerHTML = books.map(bookToLine).filter((book) => book['to-read']).join(' ');
+            document.getElementById('wishes').innerHTML = books.map(bookToLine).filter((book) => book['wishes']).join(' ');
+            document.getElementById('lended').innerHTML = books.map(bookToLine).filter((book) => book['lended']).join(' ');
+            document.getElementById('to-kend').innerHTML = books.map(bookToLine).filter((book) => book['to-kend']).join(' ');
+        });
 }
+
 getBooks();
-document.getElement
